9316e463173f5d6fab3746bf659da7c9447b9afe
unknown <msuch92@gmail.com>
2020-08-01 11:30:44 +0200

    reformated sign check testing

--- test/available_functions_test.py
+++ test/available_functions_test.py
@@ -4,58 +4,59 @@ from available_functions import OneFunction, Backspace, Character
 from user_handler import UserHandler
 
 class FunctionsTest(unittest.TestCase):
-    '''
+    """
     to be tested from terminal
 
-    '''
-
-    functions_dic = {
-        'backspace': set_backspace,
-        'character': set_character
-
-    }
+    """s
 
     # setup tests
     def setUp(self):
         stdscr = curses.initscr()
         self.user = UserHandler(stdscr)
+        self.functions_dic = {
+            'backspace': self.set_backspace,
+            'character': self.set_character
+        }
 
     # end test
     def tearDown(self):
         curses.endwin()
 
     # set backspace as function
-    def set_backspace():
+    def set_backspace(self):
         self.function = Backspace(self.user)
-    #set character as function
-    def set_character():
+
+    # set character as function
+    def set_character(self):
         self.function = Character(self.user)
+
+    # run selected function selection
+    def run_function(self, name):
+        self.functions_dic[name]()
 
     # check if selected func return correct result for given int value
     def check_int(self, func_name, int_value, result):
-        pass
-
+        self.run_function(func_name)
+        if result:
+            self.assertTrue(self.function.check_sign(int_value))
+        else:
+            self.assertFalse(self.function.check_sign(int_value))
 
     # test backspace function with correct int
     def test_backspace_int_true(self):
-        self.function = Backspace(self.user)
-        self.assertTrue(self.function.check_sign(8))  # backspace
+        self.check_int('backspace', 8, True)  # 8 - backspace
 
     # test backspace function with incorrect int
     def test_backspace_int_false(self):
-        self.function = Backspace(self.user)
-        self.assertFalse(self.function.check_sign(97))  # 97 - 'a'
+        self.check_int('backspace', 97, False)  # 97 - a
 
     # test character input function with correct input
     def test_character_true(self):
-        self.function = Character(self.user)
-        self.assertTrue(self.function.check_sign(97))  # 97 - 'a'
+        self.check_int('character', 97, True)  # 97 - a
 
     # test character input function with incorrect input
     def test_character_false(self):
-        self.function = Character(self.user)
-        self.assertFalse(self.function.check_sign(8))  # 8 - backspace
-
+        self.check_int('character', 8, False)  # 8 - backspace
 
     # test backspace output
     def test_backspace_output(self):
