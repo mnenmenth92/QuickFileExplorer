9c465234616f98b25d97b1e66beb4bd978152702
unknown <msuch92@gmail.com>
2020-08-01 14:52:40 +0200

    color pair example in comment

--- quick_file_explorer.py
+++ quick_file_explorer.py
@@ -14,6 +14,10 @@ class QuickFileExplorer:
         self.user_handler = UserHandler(stdscr)
         self.folder_handler = FolderHandler(default_path)
 
+        # Temporary
+        # self.user_handler.curses.init_pair(1, self.user_handler.curses.COLOR_YELLOW,self.user_handler.curses.COLOR_BLACK)
+        # self.user_handler.window.attron(self.user_handler.curses.color_pair(1))
+
     def one_loop_cycle(self):
         self.user_handler.select_action()
         self.user_handler.window.refresh()
