042fc2c48c77c2c646ff4ca272569d8b5bdff017
unknown <msuch92@gmail.com>
2020-08-07 13:23:05 +0200

    rest of availble function test refactored

--- test/available_functions_test.py
+++ test/available_functions_test.py
@@ -2,11 +2,11 @@ import unittest
 import curses
 import importlib
 import inspect
-from available_functions import Backspace, Character
 from user_handler import UserHandler
 from folder_handler import FolderHandler
 from display_handler import DisplayHandler
 from config import default_path
+
 
 class FunctionsTest(unittest.TestCase):
     """
@@ -17,6 +17,8 @@ class FunctionsTest(unittest.TestCase):
     # setup tests
     def setUp(self):
         stdscr = curses.initscr()
+        curses.start_color()
+
         self.folder_handler = FolderHandler(default_path)
         self.display_handler = DisplayHandler(self.folder_handler, stdscr)
         self.user = UserHandler(self.folder_handler, self.display_handler, stdscr)
@@ -26,12 +28,9 @@ class FunctionsTest(unittest.TestCase):
 
             self.functions_dic.update({type(function).__name__: function})
 
-
-
     # end test
     def tearDown(self):
         curses.endwin()
-
 
     # check if selected func return correct result for given int value
     def check_int(self, func_name, int_value, result):
@@ -59,18 +58,21 @@ class FunctionsTest(unittest.TestCase):
 
     # test backspace output
     def test_backspace_output(self):
-        self.user.input_string = 'test_string'
-        self.function = self.functions_dic['Backspace']
-        self.function.run(8)
+        function = self.functions_dic['Backspace']
+        function.user_handler = self.user
+        function.user_handler.input_string = 'test_string'
+        function.user_handler.selected_element = 1
+        function.run(8)
         self.assertEqual(self.user.input_string, 'test_strin')
 
     # test backspace output
     def test_character_output(self):
-        self.user.input_string = 'test_string'
-        self.function = Character(self.user)
-        self.function.run(97)
+        function = self.functions_dic['Character']
+        function.user_handler = self.user
+        function.user_handler.input_string = 'test_string'
+        function.user_handler.selected_element = 1
+        function.run(97)
         self.assertEqual(self.user.input_string, 'test_stringa')
-
 
 
 if __name__ == '__main__':
