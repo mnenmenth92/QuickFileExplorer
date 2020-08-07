c11caa8e52d39be97a0a1524946881bb74fd3335
unknown <msuch92@gmail.com>
2020-08-07 08:22:55 +0200

    available functions test refactor, 2 testoutput errors

--- test/available_functions_test.py
+++ test/available_functions_test.py
@@ -1,7 +1,12 @@
 import unittest
 import curses
-from available_functions import OneFunction, Backspace, Character
+import importlib
+import inspect
+from available_functions import Backspace, Character
 from user_handler import UserHandler
+from folder_handler import FolderHandler
+from display_handler import DisplayHandler
+from config import default_path
 
 class FunctionsTest(unittest.TestCase):
     """
@@ -12,56 +17,50 @@ class FunctionsTest(unittest.TestCase):
     # setup tests
     def setUp(self):
         stdscr = curses.initscr()
-        self.user = UserHandler(stdscr)
-        self.functions_dic = {
-            'backspace': self.set_backspace,
-            'character': self.set_character
-        }
+        self.folder_handler = FolderHandler(default_path)
+        self.display_handler = DisplayHandler(self.folder_handler, stdscr)
+        self.user = UserHandler(self.folder_handler, self.display_handler, stdscr)
+        self.functions_dic = {}
+        for name, cls in inspect.getmembers(importlib.import_module("available_functions"), inspect.isclass):
+            function = cls(self)
+
+            self.functions_dic.update({type(function).__name__: function})
+
+
 
     # end test
     def tearDown(self):
         curses.endwin()
 
-    # set backspace as function
-    def set_backspace(self):
-        self.function = Backspace(self.user)
-
-    # set character as function
-    def set_character(self):
-        self.function = Character(self.user)
-
-    # run selected function selection
-    def run_function(self, name):
-        self.functions_dic[name]()
 
     # check if selected func return correct result for given int value
     def check_int(self, func_name, int_value, result):
-        self.run_function(func_name)
+        function = self.functions_dic[func_name]
         if result:
-            self.assertTrue(self.function.check_sign(int_value))
+            self.assertTrue(function.check_sign(int_value))
         else:
-            self.assertFalse(self.function.check_sign(int_value))
+            self.assertFalse(function.check_sign(int_value))
 
     # test backspace function with correct int
     def test_backspace_int_true(self):
-        self.check_int('backspace', 8, True)  # 8 - backspace
+        self.check_int('Backspace', 8, True)  # 8 - backspace
 
     # test backspace function with incorrect int
     def test_backspace_int_false(self):
-        self.check_int('backspace', 97, False)  # 97 - a
+        self.check_int('Backspace', 97, False)  # 97 - a
 
     # test character input function with correct input
     def test_character_true(self):
-        self.check_int('character', 97, True)  # 97 - a
+        self.check_int('Character', 97, True)  # 97 - a
 
     # test character input function with incorrect input
     def test_character_false(self):
-        self.check_int('character', 8, False)  # 8 - backspace
+        self.check_int('Character', 8, False)  # 8 - backspace
 
     # test backspace output
     def test_backspace_output(self):
         self.user.input_string = 'test_string'
-        self.function = Backspace(self.user)
+        self.function = self.functions_dic['Backspace']
         self.function.run(8)
         self.assertEqual(self.user.input_string, 'test_strin')
 
